a6467797079dfa3c0d82ea31e0de4ffa7c5032ea
Michael Neely <michael@michaelneely.net>
2019-09-16 20:56:05 -0400

    Header for main plugin page

---
+++ woocommerce-lightbox.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Lightbox
+ * Plugin URI: http://woocommerce.com/products/woocommerce-extension/
+ * Description: Your extension's description text.
+ * Version: 1.0.0
+ * Author: Your Name
+ * Author URI: http://yourdomain.com/
+ * Developer: Your Name
+ * Developer URI: http://yourdomain.com/
+ * Text Domain: woocommerce-extension
+ * Domain Path: /languages
+ *
+ * Woo: 12345:342928dfsfhsf8429842374wdf4234sfd
+ * WC requires at least: 2.2
+ * WC tested up to: 2.3
+ *
+ * Copyright: © 2009-2015 WooCommerce.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
